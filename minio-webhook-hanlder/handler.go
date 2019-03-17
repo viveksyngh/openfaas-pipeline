569be433df546b6359cdf4283cbd806c751c41de
Vivek Singh <vivekkmr45@yahoo.in>
2019-03-17 14:29:22 +0530

    Invoke inception function with callback url
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- minio-webhook-hanlder/handler.go
+++ minio-webhook-hanlder/handler.go
@@ -1,11 +1,20 @@
 package function
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Jeffail/gabs"
+)
+
+const (
+	defaultS3URL      = "minio:9000"
+	defaultGatewayURL = "gateway:8080"
 )
 
 //RequestBody request body for resizer function
@@ -25,6 +34,12 @@ func Handle(req []byte) string {
 
 	bucket, _ := record.Path("s3.bucket.name").Data().(string)
 	objectName, _ := record.Path("s3.object.key").Data().(string)
+
+	err = invokeInception(bucket, objectName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: "+err.Error())
+	}
+
 	requestBody := RequestBody{
 		Bucket:    bucket,
 		ObjectKey: objectName,
@@ -32,4 +47,39 @@ func Handle(req []byte) string {
 	requestBytes, _ := json.Marshal(requestBody)
 
 	return fmt.Sprintf("Hello, Go. You said: %s", string(requestBytes))
+}
+
+func invokeInception(bucket, objectKeystring string) error {
+	s3URL := os.Getenv("s3_url")
+	if len(s3URL) == 0 {
+		s3URL = defaultS3URL
+	}
+
+	gatewayHostname := os.Getenv("gateway_url")
+	if gatewayHostname == "" {
+		gatewayHostname = "gateway:8080"
+	}
+
+	ext := filepath.Ext(objectKeystring)
+	filename := strings.TrimSuffix(objectKeystring, ext) + ".json"
+
+	imageURL := fmt.Sprintf("http://%s/%s/%s", s3URL, bucket, objectKeystring)
+	inceptionFnAsyncURL := fmt.Sprintf("http://%s/async-function/inception", gatewayHostname)
+	callbackFunctionURL := fmt.Sprintf("http://%s/function/json-uploader?filename=%s", gatewayHostname, filename)
+
+	reader := bytes.NewReader([]byte(imageURL))
+	client := http.Client{}
+	request, err := http.NewRequest("POST", inceptionFnAsyncURL, reader)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("X-Callback-url", callbackFunctionURL)
+
+	res, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintln(os.Stderr, "Success: "+res.Status)
+	return nil
 }
