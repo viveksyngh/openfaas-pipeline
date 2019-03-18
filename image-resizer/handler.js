7c5214a547a7bc56fc54e5fd44ad070847c7ebd4
Vivek Singh <vivekkmr45@yahoo.in>
2019-03-18 14:43:46 +0530

    Complete image-resizer function
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- image-resizer/handler.js
+++ image-resizer/handler.js
@@ -2,12 +2,14 @@
 
 var Minio = require('minio');
 var sharp = require('sharp');
+var fs = require("fs");
 
 var endpoint = process.env.s3_url || "minio";
 var port = process.env.port || 9000;
 var secretPath = process.env.secret_path || "/var/openfaas/secrets/";
-var accessKey = fs.readFileSync(secretPath + "s3-access-key");
-var secretKey = fs.readFileSync(secretPath + "s3-secret-key");
+var accessKey = fs.readFileSync(secretPath + "s3-access-key").toString();
+var secretKey = fs.readFileSync(secretPath + "s3-secret-key").toString();
+var destinationBucket = process.env.destination_bucket || "images-thumbnail";
 
 var mcConfig = {
     endPoint: endpoint,
@@ -22,11 +24,31 @@ var transformer = sharp().resize(40, 40)
 const imageType = 'image/jpg';
 
 module.exports = (context, callback) => {
-    parseContext = JSON.parse(context)
-    var bucket = parseContext.bucket;
-    var objectKey = parseContext.objectKey;
+    var parsedContext = JSON.parse(context);
+    var bucket = parsedContext.bucket;
+    var objectKey = parsedContext.objectKey;
+    console.error(bucket, objectKey)
 
-    console.log(bucket, objectKey)
+    mc.getObject(bucket, 
+            objectKey,
+            function(err, data) {
+                if (err) {
+                    console.error(err);
+                    callback(undefined, {status: "failed", message: err.toString()});
+                }
+                var reiszedFileName = objectKey.split('.')[0] + "-40x40.jpg"
+                
+                mc.putObject(destinationBucket, 
+                            reiszedFileName, 
+                            data.pipe(transformer),
+                            imageType,
+                            function(err, etag){
+                                if (err) {
+                                    console.error(err);
+                                    callback(undefined, {status: "fail", message: err.toString()});
+                                }
 
-    callback(undefined, {status: "done"});
+                                callback(undefined, {status: "success", message: "Successfully resized and uploded"});
+                            });
+    });
 }
